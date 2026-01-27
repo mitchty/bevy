1ef1077fb52098338482c37f518edfc3de598521
Mitchell Tishmack <git@mitchty.net>
2026-01-26 21:26:18 -0600

    Add padding for color_plane widget for wasm/webgl compat

--- crates/bevy_feathers/src/controls/color_plane.rs
+++ crates/bevy_feathers/src/controls/color_plane.rs
@@ -84,6 +84,10 @@ struct ColorPlaneMaterial {
 
     #[uniform(0)]
     fixed_channel: f32,
+
+    #[cfg(target_arch = "wasm32")]
+    #[uniform(0)]
+    _webgl2_padding: Vec3,
 }
 
 impl From<&ColorPlaneMaterial> for ColorPlaneMaterialKey {
@@ -207,6 +211,8 @@ fn update_plane_color(
             let material = r_materials.add(ColorPlaneMaterial {
                 plane: *plane,
                 fixed_channel: plane_value.0.z,
+                #[cfg(target_arch = "wasm32")]
+                _webgl2_padding: Vec3::ZERO,
             });
             commands.entity(*inner_ent).insert(MaterialNode(material));
         }
